59e93b2c6faf77d73685ec1ea47dfcc3fcc02749
unknown <danilo.cecilia@gmail.com>
2019-01-09 22:42:15 -0800

    Add mongoose package to project

--- FindMyBandoServer/app.js
+++ FindMyBandoServer/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect('mongodb://admin:teste123@ds153394.mlab.com:53394/fmbando');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
